cde7a56f07433f9a3485cd85a0cc69ac6f7b7c04
ArryArt09 <746785@student.dusd.net>
2026-02-04 13:48:20 -0800

    Move
    I finished about 75% of the game so far, all that's left is to design a level, multiplayer, and I'm done! Work on the GDD!

---
+++ P6GarzaUnit1PlayerControl/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 7, -14);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = player.transform.position + offset;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ P6GarzaUnit1PlayerControl/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 20.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The videos take so long for things that I already know what to do
+        //I'm leaving a comment because this is what makes the car move forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
