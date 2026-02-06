80e7ca9c41b8204590a700c481231606e1338dea
ArryArt09 <746785@student.dusd.net>
2026-02-06 13:51:58 -0800

    Drivee
    I can move! Finish GDD and game nexct week!

--- P6GarzaUnit1PlayerControl/Assets/Scripts/PlayerController.cs
+++ P6GarzaUnit1PlayerControl/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public float turnSpeed;
+    public float driveSpeed;
     public float speed = 20.0f;
+    public float horizontalInput;
+    public float forwardInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,8 +17,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         //The videos take so long for things that I already know what to do
         //I'm leaving a comment because this is what makes the car move forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * driveSpeed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
